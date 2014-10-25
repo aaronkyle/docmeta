5d94d8ee7e9ba43310aae84ca034dfc7d2698e85
pwhipp <paul.whipp@gmail.com>
2014-10-25 16:10:52 +1000

    Added setup.py and base requirements

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+required_packages = [
+    'mezzanine',
+    'django-taggit',
+    'boto',
+    'django-categories']
+
+setup(
+    name='docmeta',
+    version= 0.1,
+    description= "Document manager. Handles searching and listing documents using metadata model objects.",
+    author='Paul Whipp',
+    author_email='paul.whipp@gmail.com',
+    url='https://github.com/cccs-web/docmeta',
+    packages=find_packages(),
+    install_requires=required_packages,
+    license='BSD')
