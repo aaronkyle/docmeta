9af7217dbc7a46fd1ae857e5b008fb427c50bca8
pwhipp <paul.whipp@gmail.com>
2014-11-13 12:07:15 +1000

    Added duplicate deletion facility (#8)

--- docmeta/urls.py
+++ docmeta/urls.py
@@ -2,14 +2,17 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 import docmeta.views as views
 
 urlpatterns = patterns(
     '',
     url(r'^list/$', views.DocumentListView.as_view(), name='document-list'),
+    url(r'^duplicates/$', views.DocumentDuplicatesView.as_view(), name='document-duplicates'),
+    url(r'^delete/(?P<pk>[\w]+)/$', login_required(views.DocumentDeleteView.as_view()), name='document-delete'),
     url(r'^category/$', views.RootCategoriesView.as_view(), name='document-category-root'),
     url(r'^category/(?P<category_slugs>[\w/-]+)/$', views.CategoryView.as_view(), name='document-category'),
-    url(r'^detail/(?P<slug>[\w_-]+)/$', views.DocumentDetailView.as_view(), name='document-detail'),
+    url(r'^detail/(?P<slug>[~\w_-]+)/$', views.DocumentDetailView.as_view(), name='document-detail'),
     url(r'^bibtex/$$', TemplateView.as_view(template_name='docmeta/document_bibtex.html'), name='document-bibtex'),
     url(r'^download/(?P<slug>[\w_-]+)/$', views.download, name='document-download'))
--- docmeta/views.py
+++ docmeta/views.py
@@ -1,10 +1,14 @@
+from collections import defaultdict
 import os
 
 from django.http.response import Http404, HttpResponse
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import DeleteView
 from django.shortcuts import get_object_or_404
+
+from mezzanine.utils.views import paginate
 
 import docmeta.models as dm
 
@@ -22,13 +26,60 @@ class RootCategoriesView(TemplateView):
 
 class DocumentListView(ListView):
     model = dm.Document
-    paginate_by = 25
+    mezz_paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        context = super(DocumentListView, self).get_context_data(**kwargs)
+        context['document_list'] = paginate(
+            context['document_list'],
+            self.request.GET.get("page", 1),
+            self.mezz_paginate_by,
+            7)
+        return context
 
     def get_queryset(self):
         queryset = super(DocumentListView, self).get_queryset()
         if not self.request.user.is_staff:
             queryset = queryset.filter(status=dm.CONTENT_STATUS_PUBLISHED)
         return queryset
+
+
+class DocumentDuplicatesView(ListView):
+    model = dm.Document
+    mezz_paginate_by = 10
+    template_name = 'docmeta/document_duplicates.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DocumentDuplicatesView, self).get_context_data(**kwargs)
+        context['sha_document_list'] = paginate(
+            self.get_sha_document_list(context['document_list']),
+            self.request.GET.get("page", 1),
+            self.mezz_paginate_by,
+            7)
+        return context
+
+    @staticmethod
+    def get_sha_document_list(documents):
+        sha_dict = defaultdict(list)
+        for document in documents:
+            if document.sha is None:
+                document.update_sha()
+                document.save()
+            sha_dict[document.sha[:8]].append(document)
+
+        # Remove all entries with just one document
+        for sha, documents in list(sha_dict.items()):  # list avoids mutating dict while iterating it
+            if not documents[1:]:
+                del sha_dict[sha]
+
+        return sha_dict.items()
+
+
+class DocumentDeleteView(DeleteView):
+    model = dm.Document
+
+    def get_success_url(self):
+        return self.request.GET['next']
 
 
 class CategoryView(DocumentListView):
